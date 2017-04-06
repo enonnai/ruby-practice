f7ae3b541b7bac31faa2d72381b7db5c5a699911
Cristina Iannone <iannonecristina@gmail.com>
2017-04-06 16:40:07 +0100

    Code refactored

--- kata/kata1.rb
+++ kata/kata1.rb
@@ -1,23 +1,21 @@
 GIFTS = {1=>"Toy Soldier", 2=>"Wooden Train", 4=>"Hoop", 8=>"Chess Board", 16=>"Horse", 32=>"Teddy", 64=>"Lego", 128=>"Football", 256=>"Doll", 512=>"Rubik's Cube"}
 
 def gifts(number)
-  i = number
   result = []
 
-  if GIFTS[i] == nil
-    special_arr = (1...i).to_a
+  if GIFTS[number] == nil
+    special_arr = (1...number).to_a
 
     special_arr.each do |num|
       if GIFTS.has_key? num
         result << GIFTS[num]
       end
     end
+
   elsif
-    result << GIFTS[i]
+    result << GIFTS[number]
   end
+
   result
 end
 
-
-p gifts(1)
-p gifts(3)
