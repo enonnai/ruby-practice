f128078d471352d63eb9016e856f26403a946bd9
Cristina Iannone <iannonecristina@gmail.com>
2017-07-13 16:41:14 +0100

    Code refactoring

--- string_calculator/lib/string_calculator.rb
+++ string_calculator/lib/string_calculator.rb
@@ -1,20 +1,35 @@
 class StringCalculator
 
-  def initialize(string_numbers)
-    @string_numbers = string_numbers
+  def initialize(string)
+    @string = string
   end
 
   def add
     sum = 0
-    if @string_numbers.empty?
-      return 0
-    elsif @string_numbers.chars.count == 1 && @string_numbers !~ /\D/
-      return @string_numbers.to_i
-    elsif @string_numbers.chars.each do |char|
-        sum += char.to_i
-      end
-      return sum
+    if @string.empty?
+      return_zero
+    elsif @string.chars.count == 1 && @string !~ /\D/
+      return_single_number
+    else
+      return_sum
     end
   end
 
+  private
+
+  def return_zero
+    0
+  end
+
+  def return_single_number
+    @string.to_i
+  end
+
+  def return_sum
+    sum = 0
+    @string.chars.each do |char|
+      sum += char.to_i
+    end
+    sum
+  end
 end
--- string_calculator/spec/string_calculator_spec.rb
+++ string_calculator/spec/string_calculator_spec.rb
@@ -3,22 +3,22 @@ require 'string_calculator'
 describe StringCalculator do
   context "when a string is empty" do
     it "returns 0" do
-      string = StringCalculator.new("")
-      expect(string.add).to eq 0
+      subject = StringCalculator.new("")
+      expect(subject.add).to eq 0
     end
   end
 
   context "when a string has one number" do
     it "returns the number" do
-      string = StringCalculator.new("1")
-      expect(string.add).to eq 1
+      subject = StringCalculator.new("1")
+      expect(subject.add).to eq 1
     end
   end
 
   context "when a string has two numbers" do
     it "returns their sum" do
-      string = StringCalculator.new("1,2")
-      expect(string.add).to eq 3
+      subject = StringCalculator.new("1,2")
+      expect(subject.add).to eq 3
     end
   end
 end
